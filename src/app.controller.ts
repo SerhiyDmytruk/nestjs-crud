ed8e2f691ea60f44c54f08b4a361ec0b8f2de7f6
Dmytruk Serhii <dmitruksl92@gmail.com>
2024-01-08 21:50:17 +0200

    fix: start from scratch.

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,14 +1,19 @@
-import { Controller, Request, Post, UseGuards } from '@nestjs/common';
-import { LocalAuthGuard } from './auth/local-auth.guard';
-import { AuthService } from './auth/auth.service';
+import { Controller, Get, Res, HttpStatus } from '@nestjs/common';
+import { Response } from 'express';
+import { AppService } from './app.service';
+import * as MOCKED_RESPONSE from './data/map-list.json';
 
-@Controller()
+@Controller('map')
 export class AppController {
-  constructor(private authService: AuthService) {}
+  constructor(private readonly appService: AppService) {}
 
-  @UseGuards(LocalAuthGuard)
-  @Post('auth/login')
-  async login(@Request() req) {
-    return this.authService.login(req.user);
+  // @Get(':country')
+  // getCountry(@Param('country') country: string): string {
+  //   return this.appService.getCountry(country);
+  // }
+
+  @Get('/payment-method')
+  getPaymentMoethod(@Res() res: Response): any {
+    res.status(HttpStatus.OK).json(MOCKED_RESPONSE);
   }
 }
--- src/app.service.ts
+++ src/app.service.ts
@@ -2,7 +2,7 @@ import { Injectable } from '@nestjs/common';
 
 @Injectable()
 export class AppService {
-  getHello(): string {
-    return 'Hello World!';
+  getCountry(country: string): string {
+    return `Hello, ${country}! Welcome to NestJS!`;
   }
 }

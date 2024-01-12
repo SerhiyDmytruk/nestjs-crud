b81e29d2fc77c79d997241201127af05bc3830e1
Dmytruk Serhii <dmitruksl92@gmail.com>
2024-01-12 21:36:11 +0200

    fix: refactored app.controller

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,24 +1,14 @@
 import { Controller, Get, Res, HttpStatus } from '@nestjs/common';
 import { Response } from 'express';
-import * as MOCKED_RESPONSE from './data/map-list.json';
+import { AppService } from './app.service';
 
 @Controller('map')
 export class AppController {
-  // constructor(private readonly appService: AppService) {}
+  constructor(private readonly appService: AppService) {}
 
   @Get()
   getCountry(@Res() res: Response): any {
-    interface Country {
-      'alpha-3': string;
-      name: string;
-    }
-
-    const updatedMapArray: Country[] = MOCKED_RESPONSE.map((country) => {
-      return {
-        'alpha-3': country['alpha-3'],
-        name: country['name'],
-      };
-    });
+    const updatedMapArray = this.appService.getUpdatedMapArray();
 
     res.status(HttpStatus.OK).json(updatedMapArray);
   }
--- src/app.service.ts
+++ src/app.service.ts
@@ -1,8 +1,21 @@
 import { Injectable } from '@nestjs/common';
+import * as MOCKED_RESPONSE from './data/map-list.json';
 
 @Injectable()
 export class AppService {
-  getCountry(country: string): string {
-    return `Hello, ${country}! Welcome to NestJS!`;
+  getUpdatedMapArray(): any {
+    interface Country {
+      'alpha-3': string;
+      name: string;
+    }
+
+    const updatedMapArray: Country[] = MOCKED_RESPONSE.map((country) => {
+      return {
+        'alpha-3': country['alpha-3'],
+        name: country['name'],
+      };
+    });
+
+    return updatedMapArray;
   }
 }
